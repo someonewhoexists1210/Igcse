fe4707563f51af794e9336bccc53a56c99592c61
someonewhoexists1210 <130904011+someonewhoexists1210@users.noreply.github.com>
2024-08-27 14:03:42 +0530

    added subject codes and getting qp

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
     let form = document.getElementById('form');
+    let subject = document.getElementById('subject');
+    fetch('http://127.0.0.1:5379/subjects').then(response => response.json())
+    .then(data => {
+        console.log(data)
+        data.subjects.forEach(element => {
+            let option = document.createElement('option');
+            option.value = element.code;
+            option.text = element.name;
+            subject.add(option);
+    })
+
+
     form.addEventListener('submit', function(event) {
         event.preventDefault();
         let send = {
             code: document.getElementById('code').value,
+            qp: document.querySelector('input[name="variant"]:checked').value
         }
         console.log(send)
-        fetch('/search', {
+        fetch('http://127.0.0.1:5379/search', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -19,9 +32,8 @@ document.addEventListener('DOMContentLoaded', function() {
                 document.getElementById('result').innerHTML = data.error
                 return
             }
-            document.getElementById('result').innerHTML = data.link
-            document.getElementById('result').href = data.link
+            document.getElementById('result').src = data.link
         })
         
     });
-});
+});});
--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from dotenv import load_dotenv
 import requests
-import os
+import os, json
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
+with open('subjects.json') as f:
+    subjects = json.load(f)
+
+
 app = Flask(__name__)
-CORS(app, resources={"/search": {"origins": ["http://127.0.0.1:5500", "http://localhost"]}})
+CORS(app, resources={"*": {"origins": ["http://127.0.0.1:5500", "http://localhost"]}})
 
 
 load_dotenv()
@@ -31,7 +35,7 @@ def search_query(query):
     
     return res['items'][0]['link']
 
-def codeify(q):
+def codeify_ms(q):
     code = ''
     parts = q.split('/')
     code += parts[0] + "_"
@@ -43,16 +47,29 @@ def codeify(q):
         code += 'm' + parts[4] + '_ms_' + parts[1]
     return code
 
+def codeify_qp(q):
+    return codeify_ms(q).replace('ms', 'qp')
+
 @app.route('/search', methods=['POST'])
 def main():
     q = request.json['code']
-    q = codeify(q)
+    
+    if request.json['qp'] == 'qp':
+        q = codeify_qp(q)
+        print(q)
+    else:
+        q = codeify_ms(q)
+
     link = search_query(q)
     if link is None:
         return jsonify({'error': 'No results found'})
     else:
         print('code:', q)
         return jsonify({'link': link})
+
+@app.route('/subjects', methods=['GET'])
+def get_subjects():
+    return jsonify(subjects)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5379)
